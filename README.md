129c0e02950e25ad7dbc119323848542a20d65aa
Dinesh <s.ydinesh1177@gmail.com>
2020-06-10 19:21:28 +0530

    Initial project set-up

---
+++ README.md
@@ -0,0 +1,3 @@
+# acite_covid19
+
+An Application to overcome some issues and give self awareness to the people in COVID-19 Crisis.
---
+++ android/app/src/main/kotlin/com/example/acite_covid19/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.acite_covid19
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
